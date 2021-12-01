e4fe21cba9a77e47597e18cca5763e6df8f270c2
feizhaojun <m@feizhaojun.com>
2021-12-01 19:41:37 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Mukti
  * @Date: 2021-11-15 18:09:04
- * @LastEditTime: 2021-11-24 14:15:46
+ * @LastEditTime: 2021-11-29 15:01:06
  * @LastEditors: Mukti
  */
 import { Component, ViewChild, ElementRef } from '@angular/core';
@@ -16,6 +16,7 @@ export class AppComponent {
   checkedStatus = false;
   title = 'angular-demo';
   test = "Mukti";
+  list = ['1','2','3','4','5','6',];
 
   @ViewChild('testview')
   testview!: ElementRef<any>;
@@ -28,5 +29,16 @@ export class AppComponent {
   }
   change(event:any) {
     this.testview.nativeElement.checked = false;
+  }
+
+  ngOnInit() {
+setTimeout(() => {
+  console.log('dddddd')
+  this.list.forEach(el => {
+    return el = Number(el) + 1 + 's';
+  });
+// this.list = ['1s','2a','3s'];
+// this.list[1] = '2s';
+},1000);
   }
 }
--- src/app/life-cycle/life-cycle.component.ts
+++ src/app/life-cycle/life-cycle.component.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Mukti
  * @Date: 2021-11-23 15:38:30
- * @LastEditTime: 2021-11-25 10:24:03
+ * @LastEditTime: 2021-11-29 14:55:18
  * @LastEditors: Mukti
  */
 import { Component, Input, OnInit } from '@angular/core';
@@ -14,6 +14,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class LifeCycleComponent implements OnInit {
 
   @Input() num: any;
+  @Input() list: any;
   // num = 0;
   obj = {
     a: 1,
@@ -28,32 +29,32 @@ export class LifeCycleComponent implements OnInit {
   } //在输入属性 (input)/输出属性 (output)的绑定值发生变化时调用。
   
   ngOnInit(): void {
-    console.log('ngOnInit')
+    // console.log('ngOnInit')
   }
   
-  ngDoCheck(): void {
-    console.log('ngDoCheck')
-  } // - 开发者自定义变更检测。
+  // ngDoCheck(): void {
+  //   console.log('ngDoCheck')
+  // } // - 开发者自定义变更检测。
   
-  ngAfterContentInit(): void {
-    console.log('ngAfterContentInit')
-  } // - 在组件内容初始化后调用。
+  // ngAfterContentInit(): void {
+  //   console.log('ngAfterContentInit')
+  // } // - 在组件内容初始化后调用。
   
-  ngAfterContentChecked(): void {
-    console.log('ngAfterContentChecked')
-  } // - 在组件内容每次检查后调用。
+  // ngAfterContentChecked(): void {
+  //   console.log('ngAfterContentChecked')
+  // } // - 在组件内容每次检查后调用。
   
-  ngAfterViewInit(): void {
-    console.log('ngAfterViewInit')
-  } // - 在组件视图初始化后调用。
+  // ngAfterViewInit(): void {
+  //   console.log('ngAfterViewInit')
+  // } // - 在组件视图初始化后调用。
   
-  ngAfterViewChecked(): void {
-    console.log('ngAfterViewChecked')
-  } // - 在组件视图每次检查后调用。
+  // ngAfterViewChecked(): void {
+  //   console.log('ngAfterViewChecked')
+  // } // - 在组件视图每次检查后调用。
   
-  ngOnDestroy(): void {
-    console.log('ngOnDestroy')
-  } // - 在指令销毁前调用。
+  // ngOnDestroy(): void {
+  //   console.log('ngOnDestroy')
+  // } // - 在指令销毁前调用。
 
 
 
